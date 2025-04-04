3723f6bde3979703c7a37eb8545a19da9fd44b1b
Broly 12 <brailynperezbatista@gmail.com>
2025-04-04 15:57:02 -0400

    add AuditableEntity

---
+++ FoodTruck.Core.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace FoodTruck.Core.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public AuditableEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; } = null;
+        public string DeletedBy { get; set; } = string.Empty;
+    }
+}
