3b8db8412ad4a3dd3f35594a23b7477bd9eeda5c
Broly 12 <brailynperezbatista@gmail.com>
2025-04-07 12:19:31 -0400

    Interfaces de repositorios creadas

---
+++ FoodTruck.Infraestructure.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using FoodTruck.Core.Application.Interfaces.Persistence;
+using FoodTruck.Core.Domain.Entities;
+using FoodTruck.Infraestructure.Persistence.Base;
+using FoodTruck.Infraestructure.Persistence.Context;
+
+namespace FoodTruck.Infraestructure.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public CategoryRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
