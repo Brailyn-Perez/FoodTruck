9fad490dc3c720a97f4500815595e0bd3f5bfc05
Broly 12 <brailynperezbatista@gmail.com>
2025-04-06 21:12:17 -0400

    Soft Delete creado correctamente

--- FoodTruck.Infraestructure.Persistence/Context/ApplicationContext.cs
+++ FoodTruck.Infraestructure.Persistence/Context/ApplicationContext.cs
@@ -1,5 +1,7 @@
-﻿using FoodTruck.Core.Domain.Entities;
+﻿using FoodTruck.Core.Domain.Common;
+using FoodTruck.Core.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace FoodTruck.Infraestructure.Persistence.Context
 {
@@ -10,16 +12,60 @@ namespace FoodTruck.Infraestructure.Persistence.Context
 
         }
 
+        #region overrides methods
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Apply global query filter for soft delete
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(AuditableEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    var parameter = Expression.Parameter(entityType.ClrType, "e");
+                    var property = Expression.Property(parameter, nameof(AuditableEntity.IsDeleted));
+                    var notDeleted = Expression.Not(property);
+                    var lambda = Expression.Lambda(notDeleted, parameter);
+
+                    modelBuilder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+                }
+            }
         }
 
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        entry.Entity.CreatedBy = "DefaultAppUser";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedAt = DateTime.Now;
+                        entry.Entity.UpdatedBy = "DefaultAppUser";
+                        break;
+                    case EntityState.Deleted:
+                        entry.Entity.IsDeleted = true;
+                        entry.Entity.DeletedAt = DateTime.Now;
+                        entry.Entity.DeletedBy = "DefaultAppUser";
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        #endregion
+
+        #region DbSets
         public DbSet<Product> products { get; set; }
         public DbSet<Category> categories { get; set; }
         public DbSet<Report> reports { get; set; }
         public DbSet<User> users { get; set; }
         public DbSet<Sale> sales { get; set; }
         public DbSet<SaleDetail> saleDetails { get; set; }
+        #endregion
     }
 }
